05d22bfc0f3a59ccf178810dd4da6b62ba107e35
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2025-01-02 19:01:32 +0900

    display Posts contents from Firestore database

--- twitter-clone/src/components/timeline/Timeline.js
+++ twitter-clone/src/components/timeline/Timeline.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Timeline.css";
 import TweetBox from "./TweetBox";
 import Post from "./Post";
+import db from "../../firebase";
+import { collection, getDocs } from "firebase/firestore"; 
 
 function Timeline() {
+    const [posts, setPosts] = useState([]);
+
+    
+    useEffect(() => {
+        const postData = collection(db, "posts");
+        getDocs(postData).then((querySnapshot) => {
+            setPosts(querySnapshot.docs.map((doc => doc.data())))
+        });
+        console.log("aaa");
+    },[]);
+
   return (
   <div className="timeline">
     <div className="timeline_header">
         <h2>ホーム</h2>
     </div>
     <TweetBox />
-    <Post 
-        displayName="テストのユーザネーム"
-        username="ユーザネーム"
-        verified={true}
-        text="Reactなう"
-        avatar="https://picsum.photos/200"
-        image="https://picsum.photos/800"
-    />
+    {posts.map((post) => (
+        <Post 
+        key={post.text}
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+        />
+    ))}
   </div>
 
   );
