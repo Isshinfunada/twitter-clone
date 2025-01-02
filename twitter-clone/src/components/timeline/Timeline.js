5c3dc90fa7a2cc912610a7da4cbae6d0c089de78
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2025-01-02 13:04:00 +0900

    add: timeline component

---
+++ twitter-clone/src/components/timeline/Timeline.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Timeline.css";
+
+function Timeline() {
+  return (
+  <div className="timeline">
+    <div className="timeline_header">
+        <h2>ホーム</h2>
+    </div>
+  </div>
+
+  );
+}
+
+export default Timeline;
